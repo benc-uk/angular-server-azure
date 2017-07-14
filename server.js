8629b315bb8294beed2674a504d14de7ce567db8
Ben Coleman <benc.uk@gmail.com>
2017-07-14 10:32:41 +0100

    Much better way of serving the appsettings.js file

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 var express = require('express');
-var fs = require('fs');
-var path = require('path');
+//var fs = require('fs');
+//var path = require('path');
 var app = express();
 var public = __dirname;
 
-// We find all env vars starting APPSETTING_ and write them into appsettings.js
-// index.html should include appsettings.js, and the Angular code uses eval("FOO") to access values
-fs.writeFileSync(public + "/appsettings.js", "_APPSETTINGS = {};\n"); 
-for (var envvar in process.env) {
+// We find all env vars starting APPSETTING_ and serve them from a pseudo file appsettings.js
+// The Angular app index.html should include appsettings.js, 
+//  and the Angular code uses eval("_APPSETTINGS['FOO']") to access values
+app.get('/appsettings.js', function (req, res) {
+   var js_content = "_APPSETTINGS = {};\n"
+   for (var envvar in process.env) {
 
-   if(envvar.startsWith("APPSETTING_")) {
-      varshort = envvar.replace("APPSETTING_", "");
-      varshort = varshort.replace(" ", "_");
-      fs.appendFileSync(`${public}/appsettings.js`, `_APPSETTINGS.${varshort} = "${process.env[envvar]}";\n`); //`${varshort} = "${process.env[envvar]}";\n`);
+      if(envvar.startsWith("APPSETTING_")) {
+         var varshort = envvar.replace("APPSETTING_", "");
+         varshort = varshort.replace(" ", "_");
+         js_content += `_APPSETTINGS.${varshort} = "${process.env[envvar]}";\n`; //`${varshort} = "${process.env[envvar]}";\n`);
+      }
    }
-}
-
+   res.type('javascript')
+   res.send(js_content);
+});
 app.use('/', express.static(public));
-app.use((req, res) => res.sendFile(`${public}/index.html`))
+app.use((req, res) => res.sendFile(`${public}/index.html`));
+
 var port = process.env.PORT || 1337;
 app.listen(port);
